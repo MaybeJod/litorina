d7740fc70f1ee3b10e6c8ab8e75501d15691b5aa
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-05-06 14:42:00 +0200

    wip: testing frontend and backend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button>Click me</Button>
-    </div>
-  )
+interface Course {
+  id: string | number;
+  title: string;
 }
 
-export default App
+function App() {
+  const [courses, setCourses] = useState<Course[]>([]);
+
+  // const url = "https://litorina.onrender.com/api/courses";
+  const url = "https://litorina.onrender.com/api/categories";
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data["data"]);
+      setCourses(data["data"]);
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      {courses.map((course) => (
+        <div key={course.id} className="title">
+          {course.title}
+        </div>
+      ))}
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
