3b656aac806879c4c3bddf274cc8724b70f663a5
Priyanka <priyanka.kumari@hyperisland.se>
2025-05-09 21:08:36 +0200

    feat: Add routing to Navbar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/courses",
+    element: <div>About</div>,
+  },
+  {
+    path: "/events",
+    element: <div>Events</div>,
+  },
+  {
+    path: "/rent",
+    element: <div>Rent our space</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact</div>,
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <RouterProvider router={router} />
+    {/* <App /> */}
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
