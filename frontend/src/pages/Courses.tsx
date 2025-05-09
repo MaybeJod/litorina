3f91d5e5ca5a0d9d2c3ecaf81ca649af07fb299e
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-05-09 15:46:31 +0200

    feat: added course banner

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -1,5 +1,16 @@
 const Courses = () => {
-  return <div>courses</div>;
+  return (
+    <main className="flex flex-col items-center ">
+      {/* banner section */}
+      <section className="w-full">
+        <img
+          src="https://images.unsplash.com/photo-1543875222-8a50d1903ffc?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Courses banner"
+          className="object-cover w-full aspect-4/1"
+        />
+      </section>
+    </main>
+  );
 };
 
 export default Courses;
