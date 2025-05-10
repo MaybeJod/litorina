549b9540f52b4ec1ecbbf4360a38baa7e03685ca
Hui Chen <102537892+huichen-hc@users.noreply.github.com>
2025-05-10 11:49:41 +0200

    feat: display logo and navItems from api

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+import Logo from "./Logo";
+import { getNavigationItems } from "@/api/fetchNavigationItems";
+import type { NavigationItem } from "@/api/fetchNavigationItems";
+
 const Header = () => {
-  return <div></div>;
-  
+  const [navItems, setNavItems] = useState<NavigationItem[]>([]);
+
+  useEffect(() => {
+    const getNavItems = async () => {
+      const navData = await getNavigationItems();
+      setNavItems(navData);
+    };
+    getNavItems();
+  }, []);
+
+  return (
+    <>
+      <nav className="flex ">
+        <Logo />
+        <ul className="flex gap-5">
+          {navItems.map((item) => (
+            <li key={item.id}>
+              <a href={item.url} className="text-lg font-medium">
+                {item.title}
+              </a>
+            </li>
+          ))}
+          <li className="text-lg font-medium">SV</li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
