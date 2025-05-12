a11b24821d2af1d59c6370c1278802c0ab7b894e
odai-dh <odai.dahi@hyperisland.se>
2025-05-12 15:30:53 +0200

    refactor: add clickable cards to course card component.

--- frontend/src/components/custom/CourseCard.tsx
+++ frontend/src/components/custom/CourseCard.tsx
@@ -1,36 +1,31 @@
-import {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-} from "./CustomCourseCard";
+import { Link } from "react-router-dom";
+import { Card, CardHeader, CardFooter, CardTitle } from "./CustomCourseCard";
+import type { Course } from "@/interfaces/CourseInterface";
 
-interface CourseCardProps {
-  title: string;
-  imageUrl?: string;
-}
 
-export default function CourseCard({ title, imageUrl }: CourseCardProps) {
+export default function CourseCard({ title, imageUrl, courseId }: CourseCardProps) {
   return (
-    <Card className="w-full hover:shadow-lg transition-shadow duration-300">
-      {/* Image Section */}
-      <CardHeader className="p-0">
-        <img
-          src={imageUrl || "https://placehold.co/400"} // Placeholder image
-          alt={title}
-          className="w-full h-48 object-cover rounded-t-lg"
-        />
-      </CardHeader>
+    <Link to={`/courses/${courseId}`} className="block">
+      <Card className="w-full hover:shadow-lg transition-shadow duration-300">
+        {/* Image Section */}
+        <CardHeader className="p-0">
+          <img
+            src={imageUrl || "https://placehold.co/400"} // Placeholder image
+            alt={title}
+            className="w-full h-48 object-cover rounded-t-lg"
+          />
+        </CardHeader>
 
-      {/* Title Section */}
-      <CardFooter className="p-4">
-        <CardTitle
-          className="text-l text-center font-semibold truncate"
-          title={title}
-        >
-          {title}
-        </CardTitle>
-      </CardFooter>
-    </Card>
+        {/* Title Section */}
+        <CardFooter className="p-4">
+          <CardTitle
+            className="text-l text-center font-semibold truncate"
+            title={title}
+          >
+            {title}
+          </CardTitle>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
