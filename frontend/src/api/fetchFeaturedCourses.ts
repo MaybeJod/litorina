fb4afbf241af2e91341629d5b24faf367dc93a9b
Kevin Garcia Martin <kevingm@live.se>
2025-05-12 16:31:08 +0200

    feat: Add media

--- frontend/src/api/fetchFeaturedCourses.ts
+++ frontend/src/api/fetchFeaturedCourses.ts
@@ -2,7 +2,9 @@ import type { Course } from "@/interfaces/CourseInterface";
 
 const fetchFeaturedCourses = async (): Promise<Course[]> => {
   try {
-    const response = await fetch("https://litorina.onrender.com/api/courses");
+    const response = await fetch(
+      "https://litorina.onrender.com/api/courses?populate=*"
+    );
 
     if (!response.ok) {
       throw new Error("Failed to fetch courses");
@@ -12,12 +14,15 @@ const fetchFeaturedCourses = async (): Promise<Course[]> => {
 
     const courses = jsonData.data.map((item: any) => ({
       id: item.id,
+      documentId: item.documentId,
       title: item.title,
-      summary: item.summary,
+      media: item.media,
       isFeatured: item.isFeatured,
     }));
 
-    return courses.filter((course: Course) => course.isFeatured === true).slice(0,4);
+    return courses
+      .filter((course: Course) => course.isFeatured === true)
+      .slice(0, 4);
   } catch (error) {
     console.error("Error fetching courses:", error);
     return [];
