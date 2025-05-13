598f6f063bfbbfd162d9cd639034402853ec688c
odai-dh <odai.dahi@hyperisland.se>
2025-05-13 20:55:43 +0200

    refactor: modified the comfig files to use the new  test directory structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { configDefaults } from 'vitest/config';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +12,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setupTests.ts", // Create this file if it doesn't exist
+    exclude: [...configDefaults.exclude, "e2e/**"],
   },
 });
