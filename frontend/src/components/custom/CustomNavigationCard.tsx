771fced7711f01f1bde4654e7edf876a6a42cbb3
swarna1204 <swarnalekha.kanakaraj@hyperisland.se>
2025-05-13 15:02:06 +0200

    made changes to the home navigation card

--- frontend/src/components/custom/CustomNavigationCard.tsx
+++ frontend/src/components/custom/CustomNavigationCard.tsx
@@ -1,18 +1,14 @@
 import { Card as ShadCard } from "@/components/ui/card"; // shadcn/ui
 import type { ReactNode } from "react";
-
 function Card({ children, className = "" }: { children: ReactNode; className?: string }) {
-  return <ShadCard className={`rounded-lg overflow-hidden ${className}`}>{children}</ShadCard>;
+  return <ShadCard className={`relative rounded-sm overflow-hidden ${className}`}>{children}</ShadCard>;
 }
-
 function CardHeader({ children, className = "" }: { children: ReactNode; className?: string }) {
-  return <div className={`relative ${className}`}>{children}</div>;
+  return <div className={`${className}`}>{children}</div>;
 }
-
 function CardFooter({ children, className = "" }: { children: ReactNode; className?: string }) {
   return <div className={`p-4 ${className}`}>{children}</div>;
 }
-
 function CardTitle({
   children,
   className = "",
@@ -28,5 +24,4 @@ function CardTitle({
     </h3>
   );
 }
-
 export { Card, CardHeader, CardFooter, CardTitle };
