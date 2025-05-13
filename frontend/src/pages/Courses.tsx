ff5918e58f1fe748bcecf6735b8d8c442d891566
MaybeJod <135610495+MaybeJod@users.noreply.github.com>
2025-05-13 14:52:31 +0200

    fix: bruh skeleton on loading cards

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -123,6 +123,27 @@ const Courses = () => {
                 <Skeleton className="h-4 w-[200px]" />
               </div>
             </div>
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
+            <div className="flex flex-col space-y-3">
+              <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 w-[250px]" />
+                <Skeleton className="h-4 w-[200px]" />
+              </div>
+            </div>
           </div>
         ) : (
           <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
